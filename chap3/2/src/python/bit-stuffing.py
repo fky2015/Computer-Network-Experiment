42ea31410e0c4bed32c6eb3a084921c53d2248ec
ErvinXie <465265950@qq.com>
2020-03-25 16:28:23 +0800

    change-byte-stuffing

--- chap3/2/src/python/bit-stuffing.py
+++ chap3/2/src/python/bit-stuffing.py
@@ -5,7 +5,9 @@ info = infostring.split('=')[-1]
 flag = flagstring.split('=')[-1]
 
 print("帧起始标志：{}，帧数据信息：{}，帧结束表示{}".format(flag, info, flag))
+
 frame = flag + info.replace('11111', '111110') + flag
+
 print("比特填充之后的发送帧：{}".format(frame))
 decode = frame[len(flag):-len(flag)].replace('111110','11111')
 print("比特删除之后的接收帧：{}".format(decode))
--- chap3/2/src/python/byte-stuffing.py
+++ chap3/2/src/python/byte-stuffing.py
@@ -2,15 +2,43 @@ import re
 
 infostring = input()
 flagstring = input()
-# escstring = input()
+escstring = input()
 
 info = infostring.split('=')[-1]
 flag = flagstring.split('=')[-1]
-# esc = escstring.split('=')[-1]
-esc = '10'
+esc = escstring.split('=')[-1]
 
 print("帧起始标志：{}，帧数据信息：{}，帧结束表示{}".format(flag, info, flag))
-frame = flag + re.sub(r'({}|{})'.format(flag, esc), r'{}\1'.format(esc), info) + flag
+
+
+def encode(s):
+    re = flag
+    for i in range(len(info) - 2):
+        if i % 2 == 0:
+            x = info[i:i + 2]
+            if x == flag or x == esc:
+                re += esc
+            re += x
+    re += flag
+    return re
+
+
+def decode(frame):
+    re = ''
+    frame = frame[len(flag):-len(flag)]
+    e = True
+    for i in range(len(frame) - 2):
+        if i % 2 == 0:
+            x = frame[i:i + 2]
+            if x == esc and e :
+                continue
+            re += x
+            e = True
+    return re
+
+
+frame = encode(info)
+
 print("字节填充之后的发送帧：{}".format(frame))
-decode = re.sub(r'{}({}|{})'.format(esc, esc, flag), r'\1', frame[len(flag):-len(flag)])
-print("字节删除之后的接收帧：{}".format(decode))
+
+print("字节删除之后的接收帧：{}".format(decode(frame)))
